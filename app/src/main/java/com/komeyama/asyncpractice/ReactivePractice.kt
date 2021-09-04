4eb05f661065242c0abda9545e538c1d396a974d
YoneyamaShunpei <yoneyamashunpei@gmail.com>
2021-09-05 00:31:42 +0900

    add: flowable practice method

--- app/src/main/java/com/komeyama/asyncpractice/ReactivePractice.kt
+++ app/src/main/java/com/komeyama/asyncpractice/ReactivePractice.kt
@@ -1,6 +1,6 @@
 package com.komeyama.asyncpractice
 
-import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.core.*
 
 class ReactivePractice {
 
@@ -13,5 +13,22 @@ class ReactivePractice {
             }
             emitter.onComplete()
         }
+    }
+
+    fun flowableStringList(backpressureType: BackpressureStrategy): Flowable<String> {
+        return Flowable.create({ emitter ->
+            val messages = listOf(
+                "hello world!",
+                "hello world!!",
+                "hello world!!!",
+                "hello world!!!!",
+                "hello world!!!!!"
+            )
+            messages.forEach { message ->
+                if (emitter.isCancelled) return@create
+                emitter.onNext(message)
+            }
+            emitter.onComplete()
+        }, backpressureType)
     }
 }
